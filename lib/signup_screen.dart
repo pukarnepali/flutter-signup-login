c3ffb6f76d273bd804992ab4e1f407d9e6bc1dc3
Pukar Nepali <akaszn16@gmail.com>
2024-05-26 18:06:33 +0545

    use of validator

--- lib/signup_screen.dart
+++ lib/signup_screen.dart
@@ -8,104 +8,153 @@ class SignupScreen extends StatefulWidget {
 }
 
 class _SignupScreenState extends State<SignupScreen> {
+  var _formKey = GlobalKey<FormState>();
   bool isVisible = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
         padding: EdgeInsets.all(12.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "Welcome to Signup",
-              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(
-              height: 14,
-            ),
-            TextField(
-              decoration: InputDecoration(
-                prefixIcon: Icon(Icons.person),
-                labelText: "Name",
-                hintText: "Pukar Nepali",
-                border: OutlineInputBorder(),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Welcome to Signup",
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
               ),
-            ),
-            SizedBox(height: 15),
-            TextField(
-              decoration: InputDecoration(
-                prefixIcon: Icon(Icons.location_history),
-                labelText: "Address",
-                hintText: "Pokhara, Kaski",
-                border: OutlineInputBorder(),
+              SizedBox(
+                height: 14,
               ),
-            ),
-            SizedBox(height: 15),
-            TextField(
-              decoration: InputDecoration(
-                prefixIcon: Icon(Icons.email),
-                labelText: "Email",
-                hintText: "example@gmail.com",
-                border: OutlineInputBorder(),
+              TextFormField(
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.person),
+                  labelText: "Name",
+                  hintText: "Pukar Nepali",
+                  border: OutlineInputBorder(),
+                ),
+                validator: (String? value) {
+                  if (value == null || value.isEmpty) {
+                    return "Please enter your name";
+                  } else if (value.length < 8) {
+                    return "Name should be atleast of 8 letters";
+                  }
+                  return null;
+                },
               ),
-            ),
-            SizedBox(height: 15),
-            TextField(
-              obscureText: isVisible,
-              decoration: InputDecoration(
-                suffixIcon: IconButton(
+              SizedBox(height: 15),
+              TextFormField(
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.location_history),
+                  labelText: "Address",
+                  hintText: "Pokhara, Kaski",
+                  border: OutlineInputBorder(),
+                ),
+                validator: (String? value) {
+                  if (value == null || value.isEmpty) {
+                    return "Please enter your address";
+                  } else if (value.length < 8) {
+                    return "Enter Valid Address";
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 15),
+              TextFormField(
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.email),
+                  labelText: "Email",
+                  hintText: "example@gmail.com",
+                  border: OutlineInputBorder(),
+                ),
+                validator: (String? value) {
+                  if (value == null || value.isEmpty) {
+                    return "Please enter your name";
+                  } else if (value.length < 8) {
+                    return "Name should be atleast of 8 letters";
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 15),
+              TextFormField(
+                obscureText: isVisible,
+                decoration: InputDecoration(
+                  suffixIcon: IconButton(
+                      onPressed: () {
+                        isVisible = !isVisible;
+                        setState(() {});
+                      },
+                      icon: Icon(Icons.remove_red_eye)),
+                  prefixIcon: Icon(Icons.key),
+                  labelText: "Password",
+                  hintText: "********",
+                  border: OutlineInputBorder(),
+                ),
+                validator: (String? value) {
+                  if (value == null || value.isEmpty) {
+                    return "Please enter your Password";
+                  } else if (value.length < 8) {
+                    return "Password should be atleast of 8 letters";
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 15),
+              TextFormField(
+                obscureText: isVisible,
+                decoration: InputDecoration(
+                  suffixIcon: IconButton(
+                      onPressed: () {
+                        isVisible = !isVisible;
+                        setState(() {});
+                      },
+                      icon: Icon(Icons.remove_red_eye)),
+                  prefixIcon: Icon(Icons.key),
+                  labelText: "Confirm Password",
+                  hintText: "********",
+                  border: OutlineInputBorder(),
+                ),
+                validator: (String? value) {
+                  if (value == null || value.isEmpty) {
+                    return "Please enter your Password";
+                  } else if (value.length < 8) {
+                    return "Password should be atleast of 8 letters";
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 15),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Color.fromARGB(255, 3, 36, 184),
+                    foregroundColor: Colors.white),
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text("Form is validated")));
+                  }
+                },
+                child: Text("Create Account"),
+              ),
+              SizedBox(
+                height: 8,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Already have an accout"),
+                  TextButton(
                     onPressed: () {
-                      isVisible = !isVisible;
-                      setState(() {});
+                      Navigator.pop(context);
                     },
-                    icon: Icon(Icons.remove_red_eye)),
-                prefixIcon: Icon(Icons.key),
-                labelText: "Password",
-                hintText: "********",
-                border: OutlineInputBorder(),
-              ),
-            ),
-            SizedBox(height: 15),
-            TextField(
-              obscureText: isVisible,
-              decoration: InputDecoration(
-                suffixIcon: IconButton(
-                    onPressed: () {
-                      isVisible = !isVisible;
-                      setState(() {});
-                    },
-                    icon: Icon(Icons.remove_red_eye)),
-                prefixIcon: Icon(Icons.key),
-                labelText: "Confirm Password",
-                hintText: "********",
-                border: OutlineInputBorder(),
-              ),
-            ),
-            SizedBox(height: 15),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                  backgroundColor: Color.fromARGB(255, 3, 36, 184),
-                  foregroundColor: Colors.white),
-              onPressed: () {},
-              child: Text("Create Account"),
-            ),
-            SizedBox(
-              height: 8,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text("Already have an accout"),
-                TextButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  child: Text("Login"),
-                )
-              ],
-            )
-          ],
+                    child: Text("Login"),
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
